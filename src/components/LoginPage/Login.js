b79b36bc4292b101631c03e5ea9a6745ded4be08
Ksubi1x <xksubi1x@gmail.com>
2023-04-28 02:43:03 -0500

    cli updates

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Input from '../input/Input'
 import Button from '../Button/Button'
 import Button2 from '../SignupButton/Button2'
 import { useState, useContext } from 'react'
@@ -29,7 +28,7 @@ const Login = () => {
   const {setAuthState} = useContext(AuthContext)
 
   const onSubmit = (data) => {
-    axios.post('https://mybook12.herokuapp.com/auth/login', data).then( (response) => {
+    axios.post('https://localhost:3001/auth/login', data).then( (response) => {
       if (response.data.error) {
         alert(response.data.error)}
       else {
